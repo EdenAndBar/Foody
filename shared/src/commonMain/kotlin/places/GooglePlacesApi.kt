0efee13dbef834b8fa6815f70ea89f675b15873c
bardavidi <bardavidi12@gmail.com>
2025-06-09 18:24:48 +0300

    Test on photos

--- shared/src/commonMain/kotlin/places/GooglePlacesApi.kt
+++ shared/src/commonMain/kotlin/places/GooglePlacesApi.kt
@@ -1,7 +1,5 @@
 package places
 
-import io.ktor.client.*
-import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlinx.serialization.*
@@ -32,15 +30,16 @@ fun buildPhotoUrl(photoReference: String): String {
             "&key=AIzaSyD_EBDLvG2nhD9qDkyAp9Tm6k8-fFVfKL0"
 }
 
-suspend fun searchRestaurants(): List<String> {
+suspend fun searchRestaurants(): List<Pair<String, String>> {
     val client = getHttpClient()
 
-    val response: HttpResponse = client.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json") {
-        parameter("location", "32.0853,34.7818")
-        parameter("radius", "1500")
-        parameter("type", "restaurant")
-        parameter("key", "AIzaSyD_EBDLvG2nhD9qDkyAp9Tm6k8-fFVfKL0")
-    }
+    val response: HttpResponse =
+        client.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json") {
+            parameter("location", "32.0853,34.7818")
+            parameter("radius", "1500")
+            parameter("type", "restaurant")
+            parameter("key", "AIzaSyD_EBDLvG2nhD9qDkyAp9Tm6k8-fFVfKL0")
+        }
 
     val responseBody = response.bodyAsText()
     val parsed = json.decodeFromString<PlacesResponse>(responseBody)
@@ -48,7 +47,7 @@ suspend fun searchRestaurants(): List<String> {
     return parsed.results.mapNotNull { place ->
         val photoReference = place.photos?.firstOrNull()?.photoReference
         if (photoReference != null) {
-            buildPhotoUrl(photoReference)
+            Pair(place.name, buildPhotoUrl(photoReference))
         } else {
             null
         }
